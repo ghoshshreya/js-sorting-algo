2886f0a04c439c521d67531ab0dad7b817fcc1ca
ghoshshreya <shrghosh08@gmail.com>
2023-05-13 16:41:40 +0530

    1 file modified

--- index.js
+++ index.js
@@ -4,3 +4,35 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+function swap(arr, i, j) {
+  let temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
+function selectionSort(arr) {
+  const len = arr.length;
+  for (let i = 0; i < len - 1; i++) {
+    let min = i;
+    for (let j = i; j < len; j++) {
+      if (arr[j] < arr[min]) min = j;
+    }
+    swap(arr, i, min);
+  }
+  return arr;
+}
+console.log('Selection Sort --', selectionSort([13, 46, 24, 52, 20, 9]));
+
+function bubbleSort(arr) {
+  let i = 0;
+  for (let i = 0; i < arr.length - 1; i++) {
+    let j = 0;
+    for (let j = 0; j < arr.length - i; j++) {
+      if (arr[j] > arr[j + 1]) swap(arr, j, j + 1);
+    }
+  }
+  return arr;
+}
+
+console.log('Bubble Sort --', bubbleSort([46, 13, 24, 52, 20, 9]));
